bf2a97e1832b1220a11e715f5149d169edb81778
Charlie Unfricht <dzcharlie@gmail.com>
2025-04-11 02:29:34 -0500

    Add NPC clone from

--- YamlToWz/YamlNpcOverride.cs
+++ YamlToWz/YamlNpcOverride.cs
@@ -13,30 +13,52 @@ namespace YamlToWz
         public static List<string> WZ_FILES = ["String.wz", "Npc.wz"];
         public string Path { get; set; }
         public int Id { get; set; }
+        public int? CloneFrom { get; set; }
 
         public YamlNpcOverride Overrides { get; set; }
 
         public void AddToWz(Dictionary<string, WzFile> wzFiles)
         {
-            var wzString = wzFiles["String.wz"];
-            var wzStringNpc = wzString.WzDirectory.GetImageByName("Npc.img").GetFromPath(Id.ToString());
-            var wzNpc = wzFiles["Npc.wz"];
-            var wzNpcImg = wzNpc.WzDirectory.GetImageByName(Id.ToString().PadLeft(7, '0') + ".img");
-            if (wzNpcImg == null || wzStringNpc == null) throw new ArgumentOutOfRangeException("no npc with id " + Id);
+            var searchId = CloneFrom.HasValue ? CloneFrom.Value : Id;
 
-            // String.wz changes
-            if (Overrides.Name != null) wzStringNpc.AddAndUpdate(new WzStringProperty("name", Overrides.Name));
-            if (Overrides.Title != null) wzStringNpc.AddAndUpdate(new WzStringProperty("func", Overrides.Title));
-            if (Overrides.Script != null)
+            var wzString = wzFiles["String.wz"];
+            var wzStringNpc = wzString.WzDirectory.GetImageByName("Npc.img").GetFromPath(searchId.ToString());
+            var wzNpc = wzFiles["Npc.wz"];
+            var wzNpcImg = wzNpc.WzDirectory.GetImageByName(searchId.ToString().PadLeft(7, '0') + ".img");
+            if (wzNpcImg == null || wzStringNpc == null) throw new ArgumentOutOfRangeException("no npc with id " + searchId);
+
+            if (CloneFrom.HasValue)
             {
-                var info = wzNpcImg.GetFromPath("info") ?? new WzSubProperty("info");
-                wzNpcImg.AddAndUpdate(info);
-                var script = new WzSubProperty("script");
-                info.AddAndUpdate(script);
-                var script0 = new WzSubProperty("0");
-                script.AddAndUpdate(script0);
-                script0.AddAndUpdate(new WzStringProperty("script", Overrides.Script));
+                WzImageProperty clonedString = wzStringNpc.DeepClone();
+                clonedString.Name = Id.ToString();
+                wzString.WzDirectory.GetImageByName("Npc.img").AddAndUpdate(clonedString);
+                wzStringNpc = clonedString;
+
+                WzImage clonedNpc = wzNpcImg.DeepClone();
+                clonedNpc.Name = Id.ToString().PadLeft(7, '0') + ".img";
+                wzNpc.WzDirectory.AddImage(clonedNpc);
+                wzNpcImg = clonedNpc;
             }
+
+            if (Overrides != null)
+            {
+                // String.wz changes
+                if (Overrides.Name != null) wzStringNpc.AddAndUpdate(new WzStringProperty("name", Overrides.Name));
+                if (Overrides.Title != null) wzStringNpc.AddAndUpdate(new WzStringProperty("func", Overrides.Title));
+
+                // Npc.wz changes
+                if (Overrides.Script != null)
+                {
+                    var info = wzNpcImg.GetFromPath("info") ?? new WzSubProperty("info");
+                    wzNpcImg.AddAndUpdate(info);
+                    var script = new WzSubProperty("script");
+                    info.AddAndUpdate(script);
+                    var script0 = new WzSubProperty("0");
+                    script.AddAndUpdate(script0);
+                    script0.AddAndUpdate(new WzStringProperty("script", Overrides.Script));
+                }
+            }
+
         }
     }
 
